002e61c014f84d469b43dfe6f377c3ec0be27b2e
tonythomasm6 <tonythomasm6@gmail.com>
2024-06-17 19:13:51 +1000

    Moved Cache invalidation in deploy-ui

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -4,3 +4,9 @@
 aws s3 rm s3://tonythomasm.com/ --recursive
 # Sync files to S3 bucket
 aws s3 cp --recursive --only-show-errors ui s3://tonythomasm.com
+
+DISTRIBUTION_ID="E36TUBQZM6END5"
+
+aws cloudfront create-invalidation \
+    --distribution-id $DISTRIBUTION_ID \
+    --paths "/*"
--- invalidate-cloudfront-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DISTRIBUTION_ID="E36TUBQZM6END5"
-
-aws cloudfront create-invalidation \
-    --distribution-id $DISTRIBUTION_ID \
-    --paths "/*"

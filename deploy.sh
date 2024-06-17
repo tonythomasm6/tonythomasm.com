f424b5f835e06f71384a93e303e66f2b245829e1
tonythomasinfo <94329816+tonythomasinfo@users.noreply.github.com>
2024-06-17 15:01:35 +1000

    Added context for aws creds

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Set AWS credentials
-
-
+# Remove existing contents
+aws s3 rm s3://tonythomasm.com/ --recursive
 # Sync files to S3 bucket
-aws s3 sync ui/ s3://tonythomasm.com/
+aws s3 cp --recursive --only-show-errors ui s3://tonythomasm.com
